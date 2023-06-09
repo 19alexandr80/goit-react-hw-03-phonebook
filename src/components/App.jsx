640e31aba39b0fe2964786b365797659b66c1759
19alexandr80 <usanka1980@gmail.com>
2023-06-09 08:55:23 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { nanoid } from 'nanoid';
+
+import { FormAddUser } from 'components/formPhoneBook/FormAddUser';
+import { Filter } from 'components/contactList/Filter';
+import { ContactList } from 'components/contactList/ContactList';
+import { AppStyled } from 'components/AppStyled.styled';
+
+export class App extends React.PureComponent {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onChangeFilter = e => {
+    this.setState(() => {
+      return { filter: e.target.value };
+    });
+  };
+
+  onDeleteUser = eId => {
+    this.setState(({ contacts }) => {
+      return {
+        contacts: contacts.filter(({ id }) => {
+          return id !== eId;
+        }),
+      };
+    });
+  };
+
+  addUserPhoneBook = add => {
+    if (this.state.contacts.find(el => el.name === add.name)) {
+      alert(add.name + ' is already in contacts');
+      return;
+    }
+
+    add.id = nanoid();
+    // this.setState({ contacts: [...this.state.contacts, add] });
+
+    this.setState(({ contacts }) => {
+      return { contacts: [add, ...contacts] };
+    });
+  };
+
+  componentDidMount() {
+    this.setState(() => {
+      return {
+        contacts: JSON.parse(localStorage.getItem('contacts'))
+          ? JSON.parse(localStorage.getItem('contacts'))
+          : [],
+      };
+    });
+  }
+
+  componentDidUpdate() {
+    const contactsJson = JSON.stringify(this.state.contacts);
+    localStorage.setItem('contacts', contactsJson);
+  }
+
+  render() {
+    const ren = this.state.contacts.filter(({ name }) => {
+      return name.toLowerCase().includes(this.state.filter.toLowerCase());
+    });
+    return (
+      <AppStyled>
+        <h1>Phonebook</h1>
+        <FormAddUser addUserPhoneBook={this.addUserPhoneBook} />
+        <h2>Contacts</h2>
+        <Filter filter={this.state.filter} onChange={this.onChangeFilter} />
+        {ren.length !== 0 && (
+          <ContactList ren={ren} onDeleteUser={this.onDeleteUser} />
+        )}
+      </AppStyled>
+    );
+  }
+}
